f3a567c93dff3905f603683a6620029485e670d5
genius-ayush <ayushsanjayrawal@gmail.com>
2025-01-30 15:37:50 +0530

    auth route without input validation

--- mizrebel_backend/dist/index.js
+++ mizrebel_backend/dist/index.js
@@ -4,8 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const auth_1 = __importDefault(require("./routers/auth"));
+const user_1 = __importDefault(require("./routers/user"));
+const product_1 = __importDefault(require("./routers/product"));
+const category_1 = __importDefault(require("./routers/category"));
+const cart_1 = __importDefault(require("./routers/cart"));
 const app = (0, express_1.default)();
 const port = 3000;
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use('/auth', auth_1.default);
+app.use('/user', user_1.default);
+app.use('/product', product_1.default);
+app.use('/category', category_1.default);
+app.use('/cart', cart_1.default);
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
