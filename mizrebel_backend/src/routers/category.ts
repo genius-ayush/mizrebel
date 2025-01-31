4ceb6d19c384620c68c7dba504245cb42880fbf3
genius-ayush <ayushsanjayrawal@gmail.com>
2025-01-31 22:56:48 +0530

    nothing today

--- mizrebel_backend/src/routers/category.ts
+++ mizrebel_backend/src/routers/category.ts
@@ -1,6 +1,29 @@
 import { Router } from "express";
-
+import { authenticateUserJwtAdmin , authenticateUserJwtUser } from "../middleware";
 const router = Router() ; 
 
+
+// create a new category(admin)
+router.post("/categories" ,authenticateUserJwtAdmin ,  (req , res)=>{
+
+    
+})
+
+
+// update a categorory(admin)
+router.patch("/categories/:id" , authenticateUserJwtAdmin , (req , res)=>{
+    
+})
+
+
+// delete a category(admin)
+router.delete("/categories/:id" , authenticateUserJwtAdmin , (req , res)=>{
+
+})
+
+// get all categories
+router.get("/categories" ,authenticateUserJwtUser , (req , res)=>{
+
+})
 
 export default router ; 
--- mizrebel_backend/src/routers/user.ts
+++ mizrebel_backend/src/routers/user.ts
@@ -3,4 +3,16 @@ import { Router } from "express";
 const router = Router() ; 
 
 
+// get user details (admin only or authenticated user)
+router.get("/users/:id" , (req , res)=>{
+    
+})
+
+
+// update user details 
+router.post("/users/:id" , (req , res)=>{
+
+})
+
+
 export default router ; 
